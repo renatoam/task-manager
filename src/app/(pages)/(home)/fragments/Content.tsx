b98aee8213dea683fa1144ecc70af8cc2a452b12
Renato <renato.melo@ab-inbev.com>
2025-06-16 19:00:11 -0300

    fix: wrap useSearchParams into Suspense

--- src/app/(pages)/(home)/fragments/Content.tsx
+++ src/app/(pages)/(home)/fragments/Content.tsx
@@ -4,13 +4,16 @@ import { CreateTask } from "features/app/features/createTask/ui";
 import { FilterTasks } from "features/app/features/filterTasks/ui";
 import { LoadTasks } from "features/app/features/loadTasks/ui";
 import { queryClient } from "features/app/shared/config/query";
+import { Suspense } from "react";
 
 export default function Content() {
   return (
     <QueryClientProvider client={queryClient}>
-      <CreateTask />
-      <LoadTasks />
-      <FilterTasks />
+      <Suspense fallback={<div>Loading...</div>}>
+        <CreateTask />
+        <LoadTasks />
+        <FilterTasks />
+      </Suspense>
     </QueryClientProvider>
   );
 }
